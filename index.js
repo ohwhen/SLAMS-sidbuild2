7bdb7c0f607a9671e8c9c424a50b836bd5e94635
Bastian Roden <broden@sidlee.com>
2013-04-15 11:52:06 +0200

    changing module ref check

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if( require.resolve( 'sidbuild2' ) ) {
+if( require.resolve( 'sidbuild2' ) && require('sidbuild2') !== this ) {
 	module.exports = require('sidbuild2');
 } else {
 	module.exports = require('./lib/sidbuild2mock');
